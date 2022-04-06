1df2db0eb335a2c69c6b4ffce5f57d7ea220ae20
Oleg Grigorev <osrolex@yandex.ru>
2022-04-06 12:00:09 +0500

    изменён слайдер, теперь пролистывается

--- js/script.js
+++ js/script.js
@@ -358,49 +358,144 @@ function calculateTime(endTime) {
 const promoCountdown = setInterval(calculateTime, 1000, endTime);
 
 // ----------------------------- Слайдер -------------------------------------------
-const slides = document.querySelectorAll('.offer__slide');
-const prev = document.querySelector('.offer__slider-prev');
-const next = document.querySelector('.offer__slider-next');
-const total = document.querySelector('#total');
-const current = document.querySelector('#current');
+// Вариант слайдера №2, посложнее
+const slides = document.querySelectorAll('.offer__slide'),
+    prev = document.querySelector('.offer__slider-prev'),
+    next = document.querySelector('.offer__slider-next'),
+    total = document.querySelector('#total'),
+    current = document.querySelector('#current'),
+    slidesWrapper = document.querySelector('.offer__slider-wrapper'),
+    slidesField = document.querySelector('.offer__slider-inner'),
+    width = window.getComputedStyle(slidesWrapper).width; // получает ширину из вычисленных цсс-стилей
+
 let slideIndex = 1;
+let offset = 0; // Счётчик отступа влево или вправо
 
-showSlides(1); // первичный запуск функции для установки первого слайда
-
-// проверка на количество слайдов, надо ли добавлять нолик для отображения перед номером слайда
+// // проверка на количество слайдов, надо ли добавлять нолик для отображения перед номером слайда
 if (slides.length < 10) {
     total.textContent = `0${slides.length}`;
+    current.textContent = `0${slideIndex}`;
 } else {
     total.textContent = slides.length;
+    current.textContent = slideIndex;
 }
 
+// ширина карусели должна быть в процентах и равна кол-ву слайдов умнож. на 100%
+slidesField.style.width = slides.length * 100 + '%';
+slidesField.style.display = 'flex';
+slidesField.style.transition = '0.5s all';
+slidesWrapper.style.overflow ='hidden';
 
-function showSlides(n) {
-    // установка индекса слайдов при выходе за допустимые границы
-    if (n > slides.length) {
+// устанавливаем ширину для каждого слайда = вычисленной ширине из цсс-стилей
+slides.forEach(slide => {
+    slide.style.width = width;
+});
+
+next.addEventListener('click', () => {
+    // width.slice(0, width.length -2) означает что мы вырезаем из строки только число, а "px" в конце отбрасываем
+    if (offset == +width.slice(0, width.length - 2) * (slides.length - 1)) { // ширина хранится в виде строки. переделываем в число.
+        offset = 0;
+    } else {
+        offset += +width.slice(0, width.length - 2);
+    }
+    slidesField.style.transform = `translateX(-${offset}px)`;
+
+    if (slideIndex == slides.length) {
         slideIndex = 1;
+    } else {
+        slideIndex++;
     }
-    if (n < 1) {
-        slideIndex = slides.length;
-    }
-    // отображение слайдов. сначала все скрываем, затем показываем нужный.
-    slides.forEach(slide => slide.style.display = 'none');
-    slides[slideIndex - 1].style.display = 'block';
-    // показывает номер текущего слайда
+
     if (slides.length < 10) {
         current.textContent = `0${slideIndex}`;
     } else {
         current.textContent = slideIndex;
     }
-}
-
-function plusSlides(n) {
-    showSlides(slideIndex += 1);
-}
+});
 
 prev.addEventListener('click', () => {
-    plusSlides(-1);
+    // width.slice(0, width.length -2) означает что мы вырезаем из строки только число, а "px" в конце отбрасываем
+    if (offset == 0) { // ширина хранится в виде строки. переделываем в число.
+        offset = +width.slice(0, width.length -2) * (slides.length - 1);
+    } else {
+        offset -= +width.slice(0, width.length - 2); // вычитаем ширину слайда
+    }
+    slidesField.style.transform = `translateX(-${offset}px)`;
+
+    if (slideIndex == 1) {
+        slideIndex = slides.length;
+    } else {
+        slideIndex--;
+    }
+
+    if (slides.length < 10) {
+        current.textContent = `0${slideIndex}`;
+    } else {
+        current.textContent = slideIndex;
+    }
+
 });
-next.addEventListener('click', () => {
-    plusSlides(1);
-});
+
+// Вариант слайдера №1, самый простой
+// используется такой хтмл:
+// <div class="offer__slider-wrapper">
+//     <div class="offer__slide">
+//         <img src="img/slider/pepper.jpg" alt="pepper">
+//     </div>
+//     <div class="offer__slide">
+//         <img src="img/slider/food-12.jpg" alt="food">
+//     </div>
+//     <div class="offer__slide">
+//         <img src="img/slider/olive-oil.jpg" alt="oil">
+//     </div>
+//     <div class="offer__slide">
+//         <img src="img/slider/paprika.jpg" alt="paprika">
+//     </div>
+// </div>
+
+// const slides = document.querySelectorAll('.offer__slide');
+// const prev = document.querySelector('.offer__slider-prev');
+// const next = document.querySelector('.offer__slider-next');
+// const total = document.querySelector('#total');
+// const current = document.querySelector('#current');
+// let slideIndex = 1;
+
+// showSlides(1); // первичный запуск функции для установки первого слайда
+
+// // проверка на количество слайдов, надо ли добавлять нолик для отображения перед номером слайда
+// if (slides.length < 10) {
+//     total.textContent = `0${slides.length}`;
+// } else {
+//     total.textContent = slides.length;
+// }
+
+
+// function showSlides(n) {
+//     // установка индекса слайдов при выходе за допустимые границы
+//     if (n > slides.length) {
+//         slideIndex = 1;
+//     }
+//     if (n < 1) {
+//         slideIndex = slides.length;
+//     }
+//     // отображение слайдов. сначала все скрываем, затем показываем нужный.
+//     slides.forEach(slide => slide.style.display = 'none');
+//     slides[slideIndex - 1].style.display = 'block';
+//     // показывает номер текущего слайда
+//     if (slides.length < 10) {
+//         current.textContent = `0${slideIndex}`;
+//     } else {
+//         current.textContent = slideIndex;
+//     }
+// }
+
+// function plusSlides(n) {
+//     showSlides(slideIndex += 1);
+// }
+
+// prev.addEventListener('click', () => {
+//     plusSlides(-1);
+// });
+// next.addEventListener('click', () => {
+//     plusSlides(1);
+// });
